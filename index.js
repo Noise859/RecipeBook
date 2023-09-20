cb7845c017d7730e178372a49f67856a9f40a79b
Noise859 <brennan.neuser@gmail.com>
2023-09-20 02:14:50 -0500

    Big huuuugeee chhhannnnggesss

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var fs = require('fs');
 
 server.listen(7777, function () {
     console.log('Server listening at port %d', port);
@@ -19,6 +20,22 @@ io.on('connection', function (socket) {
   
     console.log('User Connected');
 
-  
-  
+    socket.on("getRecipes", function(boxNum) {
+        var recipeNames = [];
+        fs.readFile('recipes.json', function (err, d) {
+            if (err) {
+              socket.emit("e", err);
+              throw err;
+            }
+            
+            var data = JSON.parse(d);
+            for(var i = 0; i < data.recipes.length; i++) {
+                console.log(data.recipes[i].name);
+                recipeNames.push(data.recipes[i].name);
+                console.log(recipeNames);
+            }
+            socket.emit("recieveRecipes", recipeNames, boxNum);
+        });
+    });
+    
 });
--- public/main.js
+++ public/main.js
@@ -1,7 +1,79 @@
-var socket = io.connect("http://127.0.0.1:7777");
+var socket = io.connect("http:/127.0.0.1:7777");
 
 function daySlide(){
-    document.getElementById("h1T").style.paddingTop = "3vh";
-    document.getElementById("h1T").style.margin = "0 3vh";
-    document.getElementById("h1T").style.fontSize = "36px";
+    if(document.getElementById('dayI').value != null && document.getElementById('dayI').value > 0 && document.getElementById('dayI').value <= 42) {
+        document.getElementById("slide1").style.marginLeft = "-10vw";
+        document.getElementById("slide2").style.marginLeft = "0";
+        document.getElementById("recipesCont").innerHTML = "";
+        for(var i=0;i<document.getElementById('dayI').value;i++) {
+            document.getElementById("recipesCont").innerHTML += `<div class="recipeCont">
+                                                                    <button onclick="addRecipe(${i})" class="addRecipe" id="addRecipe${i}">Add  Recipe ${i+1}</button>
+                                                                    <div id="singleRecipeCont${i}" style="display: none">
+                                                                        <input type="text" placeholder="Search Recipe" onfocus="showRecipes(${i});" onfocusout="hideRecipes(${i});" onkeyup="filterFunction(${i})" class="recipeSearchBox" id="recipeSearchBox${i}">
+                                                                        <ul id="loadRecipeCont${i}" class="recipeList" style="display: none;">
+                                                                        </ul>
+                                                                    </div>
+                                                                </div>`
+        }
+    }
+}
+
+function backSlide1() {
+    document.getElementById("slide1").style.marginLeft = "0";
+    document.getElementById("slide2").style.marginLeft = "100vw";
+}
+
+function backSlide2() {
+    document.getElementById("slide2").style.marginLeft = "0";
+    document.getElementById("slide3").style.marginLeft = "100vw";
+}
+
+function addRecipe(boxNum) {
+    recipeCont = document.getElementById("singleRecipeCont" + boxNum);
+    document.getElementById("addRecipe" + boxNum).style.display = "none";
+    recipeCont.style.display = "block"; 
+    recipeCont.focus();
+}
+
+function filterFunction(boxNum) {
+    var input = document.getElementById("recipeSearchBox"+boxNum).value.toUpperCase();
+    var availableRecipes = document.getElementsByClassName("recipeOpL"+boxNum);
+    for(var i = 0; i<availableRecipes.length;i++){
+        var txtValue = availableRecipes[i].textContent || availableRecipes[i].innerText;
+        if (txtValue.toUpperCase().indexOf(input) > -1) {
+            availableRecipes[i].style.display = "";
+          } else {
+            availableRecipes[i].style.display = "none";
+          }
+    }
+
+}
+
+function showRecipes(boxNum) {
+    socket.emit("getRecipes", boxNum);
+}
+
+socket.on("recieveRecipes", function(recipes, boxNum) {
+    var recipeList = document.getElementById("loadRecipeCont"+boxNum);
+    recipeList.innerHTML = "";
+    for(var i = 0;i<recipes.length;i++) {
+        recipeList.innerHTML += `<li class="recipeOpL${boxNum}" onclick="fillSearch('${recipes[i]}', ${boxNum});">${recipes[i]}</li>`
+    }
+    recipeList.style.display = "block";
+});
+
+function fillSearch(foodName, boxNum) {
+    document.getElementById("recipeSearchBox"+boxNum).value = foodName;
+    hideRecipes(boxNum);
+}
+
+function hideRecipes(boxNum) {
+    setTimeout(function(){
+        document.getElementById("loadRecipeCont"+boxNum).style.display = "none";
+    }, 200);
+}
+
+function generateLists(){
+    document.getElementById("slide2").style.marginLeft = "-10vw";
+    document.getElementById("slide3").style.marginLeft = "0";
 }
